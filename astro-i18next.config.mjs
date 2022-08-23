d5a8f3272449fc60b488d8bbb900cfe6a3b9550f
afifnandya <afifnandya@gmail.com>
2022-08-23 14:53:49 +0700

    install i18n plugin

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLanguage: "en",
+  supportedLanguages: ["en", "th"],
+  i18next: {
+    // debug is convenient during development to check for missing keys
+    debug: true,
+    initImmediate: false,
+    backend: {
+      loadPath: "./src/locales/{{lng}}.json",
+    },
+  },
+  i18nextPlugins: { fsBackend: "i18next-fs-backend" },
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), vue(), astroI18next()],
 });
