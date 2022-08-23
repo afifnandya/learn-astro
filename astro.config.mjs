c049c5d4424c2f0b19d59322a156e8bc2a51125a
afifnandya <afifnandya@gmail.com>
2022-08-23 20:58:22 +0700

    add astro image plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
-
 import vue from "@astrojs/vue";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), astroI18next()],
+  integrations: [tailwind(), vue(), astroI18next(), image()],
 });
