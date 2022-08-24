a59759688b22fe314b44fa5c25ba94e8658e40d1
afifnandya <afifnandya@gmail.com>
2022-08-24 13:33:45 +0700

    disable debug astro i18n

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -4,7 +4,7 @@ export default {
   supportedLanguages: ["en", "th"],
   i18next: {
     // debug is convenient during development to check for missing keys
-    debug: true,
+    debug: false,
     initImmediate: false,
     backend: {
       loadPath: "./src/locales/{{lng}}.json",
