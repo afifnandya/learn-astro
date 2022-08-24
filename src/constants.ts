4c451847da13c20eea617a6293ccb772f80e9428
afifnandya <afifnandya@gmail.com>
2022-08-24 13:33:24 +0700

    add constant

---
+++ src/constants.ts
@@ -0,0 +1,107 @@
+export const API_MINOR_VERSION = 3;
+export const API_MAJOR_VERSION = "v5";
+export const API_DOMAIN = import.meta.env.API_DOMAIN;
+export const API_PROVIDER = "hungryhub";
+export const API_BASE_URL = "/api";
+export const API_CLIENT_TYPE = "web";
+export const DEFAULT_AVATAR =
+  "https://d38lri8pyzrvor.cloudfront.net/mr_hungry_avatar.png";
+export const SOURCE = "website";
+export const PROVIDER = "hungryhub";
+export const UTM_SOURCE = "utm_source";
+export const UTM_MEDIUM = "utm_medium";
+export const UTM_VERSION = "utm_version";
+export const PORTAL_FOR_MODAL_SELECTOR = "portal-for-modal";
+export const ORDER_NOW = "order-now";
+export const ORDER_LATER = "order-latter";
+export const PICK_UP = "pick up";
+export const DELIVERY = "delivery";
+export const DINE_IN = "dine in";
+export const RESERVATION_PAID = "paid";
+export const RESERVATION_PENDING_ARRIVAL = "pending_arrival";
+export const RESERVATION_PENDING_CONFIRMATION = "pending_confirmation";
+export const RESERVATION_WAITING_FOR_PAYMENT = "waiting_for_payment";
+export const RESERVATION_CANCELED = "cancelled";
+export const PACKAGE_PREFERENCE_DINE_IN = "dine in package";
+export const PACKAGE_PREFERENCE_DELIVERY_PICK_UP = "delivery/pick up package";
+export const PACKAGE_PREFERENCE_XPERIENCE = "xperience package";
+export const MIN_POINT_TO_REDEEM = 200;
+export const IOS_STORE_LINK =
+  "https://apps.apple.com/in/app/hungry-hub-dining-offer-app/id879303325";
+export const ANDROID_STORE_LINK =
+  "https://play.google.com/store/apps/details?id=com.hb.hungryhub";
+export const PRODUCTION_URL = "https://web.hungryhub.com";
+export const ROUTE_HOME_PAGE = "HomePage";
+export const ROUTE_SEARCH_PAGE = "SearchPage";
+export const ROUTE_RESTAURANT_PAGE = "RestaurantPage";
+export const ROUTE_VOUCHER_PAGE = "VoucherPage";
+export const ROUTE_BUY_VOUCHER_PAGE = "BuyVoucherPage";
+export const ROUTE_BUY_RESTAURANT_VOUCHER_PAGE = "BuyRestaurantVoucherPage";
+export const ROUTE_BUY_RESTAURANT_VOUCHER_SIDEBAR =
+  "BuyRestaurantVoucherSidebar";
+export const ROUTE_BOOKING_LANDING_PAGE = "Landing";
+export const ROUTE_VOUCHER_LANDING_PAGE = "LandingVoucher";
+export const ROUTE_VOUCHER_QR_PAYMENT_LANDING_PAGE = "VoucherQRPaymentLanding";
+export const ROUTE_PROFILE_VOUCHER = "ProfileVoucher";
+export const ROUTE_PROFILE_HOME = "ProfileHome";
+export const ROUTE_PROFILE_EDIT = "ProfileEdit";
+export const ROUTE_PROFILE_PASSWORD = "ProfilePassword";
+export const ROUTE_PROFILE_FAVOURITE = "ProfileFavourite";
+export const ROUTE_PROFILE_HISTORY = "ProfileHistory";
+export const ROUTE_PROFILE_POINT = "ProfilePoint";
+export const ROUTE_PROFFILE_POINT_HISTORY = "ProfilePointHistory";
+export const ROUTE_PROFILE_BENEFITS = "ProfileBenefits";
+export const ROUTE_PAYMENT_SUCCESS = "ReservationPaymentSuccess";
+export const ROUTE_PAYMENT_FAILED = "ReservationPaymentFailed";
+export const ROUTE_PAYMENT_OTP = "ReservationPaymentOTP";
+export const ROUTE_GROUP_LANDING_PAGE = "group-landing";
+export const ROUTE_NOT_FOUND = "NotFound";
+export const PAYMENT_CREDIT_CARD = "card";
+export const PAYMENT_PROMPTPAY = "promptpay";
+export const PAYMENT_TRUEWALLET = "true_wallet";
+export const PAYMENT_SHOPEEPAY = "shopee_pay";
+export const DELIVERY_METHOD_PICKUP_AND_DELIVERY = "self_pickup_and_delivery";
+export const DELIVERY_METHOD_DELIVERY = "delivery";
+export const DELIVERY_METHOD_PICKUP = "self_pickup";
+export const PRICING_TYPE_ALA_CARTE = "ala_carte";
+export const PACKAGE_CODE_AYCE = "ayce";
+export const PACKAGE_CODE_PP = "pp";
+export const PACKAGE_CODE_HAH = "hah";
+export const PACKAGE_CODE_BFP = "bfp";
+export const PACKAGE_CODE_SM = "sm";
+export const PACKAGE_CODE_HS = "hs";
+export const PACKAGE_CODE_XP = "xp";
+export const SUPPORTED_PACKAGE_TYPE = [
+  PACKAGE_CODE_AYCE,
+  PACKAGE_CODE_PP,
+  PACKAGE_CODE_HAH,
+  PACKAGE_CODE_BFP,
+  PACKAGE_CODE_SM,
+  PACKAGE_CODE_HS,
+  PACKAGE_CODE_XP,
+];
+export const SUPPORTED_PAYMENT_PROMPT_PAY = "promptpay";
+export const SUPPORTED_PAYMENT_CREDIT_CARD = "credit_card";
+export const SUPPORTED_PAYMENT_CC = "creditcard";
+export const SUPPORTED_PAYMENT_QR = "qrcode";
+export const SUPPORTED_PAYMENT_CC_QR = "qrcode_and_creditcard";
+export const SUPPORTED_PAYMENT_SHOPEE_PAY = "shopee_pay";
+export const SUPPORTED_PAYMENT_TRUE_WALLET = "true_wallet";
+export const INVOLVE_ASIA_CLICK_ID = "ia_click_id";
+export const BOOKING_FLOW_PREFERENCE_DATE_FIRST = "date_first";
+export const BOOKING_FLOW_PREFERENCE_PACK_FIRST = "pack_first";
+export const BOOKING_PAYMENT_METHOD_CREDIT_CARD = "credit_card";
+export const BOOKING_PAYMENT_METHOD_PROMPT_PAY = "promptpay";
+export const BOOKING_PAYMENT_METHOD_TRUE_WALLET = "true_wallet";
+export const HUNGRY_HUB_VOUCHER = "hungry-hub-voucher";
+export const ERROR_INVALID_DATE = "Invalid date, please choose another date";
+export const PAYMENT_PROVIDER_OMISE = "omise";
+export const PAYMENT_PROVIDER_GB_PRIMEPAY = "gb_primepay";
+export const VOUCHER_CATEGORY_GIFT = "gift";
+export const VOUCHER_CATEGORY_REDEMPTION = "redemption";
+export const VOUCHER_USAGE_DEDUCTIBLE = "deductible";
+export const ERROR_EXPIRED_TOKEN =
+  "Your session has expired, Please login again";
+export const OFFERS_FRAME_ID = "offers-frame";
+export const PAYMENT_FRAME_ID = "payment-frame";
+export const REDEEM_POINT_VOUCHER_NAME = "REDEEMCODE";
