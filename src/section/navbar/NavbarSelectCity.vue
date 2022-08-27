9c562916b1215058c81e57b9761cb4bb42a0d5df
afifnandya <afifnandya@gmail.com>
2022-08-27 11:47:12 +0700

    try

--- src/section/navbar/NavbarSelectCity.vue
+++ src/section/navbar/NavbarSelectCity.vue
@@ -14,7 +14,7 @@ import type { City } from "@/types/City";
 import { onMounted, PropType, watch } from "vue";
 import { ref } from "vue";
 import SelectCity from "@/components/SelectCity.vue";
-import { cityOption as storeCities } from "@/stores/city";
+// import { cityOption as storeCities } from "@/stores/city";
 import { selectedCityId } from "@/stores/city";
 const props = defineProps({
   cityOption: {
@@ -31,26 +31,26 @@ function setData(newval: string) {
   selectedCityId.value = newval;
 }
 
-watch(storeCities.value, (newVal) => {
-  console.log("watch val change", newVal);
-  if (newVal.length) {
-    cities.value = [...newVal, ...newVal];
-  }
-});
-onMounted(() => {
-  console.log("on mounteed");
-  cities.value.push({
-    id: "99",
-    type: "cities",
-    attributes: {
-      name: "mm",
-      homeDescription: "lol",
-    },
-  });
-  const el = document.getElementById("bubu");
-  if (el) {
-    el.textContent = "Madara";
-  }
-});
+// watch(storeCities.value, (newVal) => {
+//   console.log("watch val change", newVal);
+//   if (newVal.length) {
+//     cities.value = [...newVal, ...newVal];
+//   }
+// });
+// onMounted(() => {
+//   console.log("on mounteed");
+//   cities.value.push({
+//     id: "99",
+//     type: "cities",
+//     attributes: {
+//       name: "mm",
+//       homeDescription: "lol",
+//     },
+//   });
+//   const el = document.getElementById("bubu");
+//   if (el) {
+//     el.textContent = "Madara";
+//   }
+// });
 // console.log("CITY FROM CLIENT", cityOption.value);
 </script>
